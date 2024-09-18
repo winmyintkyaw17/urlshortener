ad3388076b0c14ef9ba2865a900d576c735c8f1f
Win Myint Kyaw <6531503193@lamduan.mfu.ac.th>
2024-09-18 22:22:26 +0700

    second time

--- url/urls.py
+++ url/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index,name="home"),
     path('url/list', views.url_list, name="url_list"),
-    path('url/<int:id>',views.sg_url, name="sg_url" )
+    path('url/update/<int:id>',views.update_url, name="update_url" ),
+    path('url/delete/<int:id>',views.delete_url, name="delete_url")
 ]
--- url/views.py
+++ url/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from .models import Url
 from hashlib import md5
+from django.contrib import messages
 # Create your views here.
 def url_list(request):
     urls = Url.objects.all()
@@ -11,13 +12,50 @@ def url_list(request):
     return render(request, "url_list.html", {
         "urls": urls
     })
-def sg_url(request, id):
-    return render(request, "sg_url.html")
+def delete_url(request, id):
+    if request.method == "POST":
+        url = get_object_or_404(Url, pk=id)
+        return HttpResponse("delete")
+def update_url(request, id):
+    url = get_object_or_404(Url, pk=id)
+    if request.method == "POST":
+        #init status
+        status =False
+        req_url = request.POST.get("url")
+
+        check_url = URLValidator()
+
+        try:
+            check_url(req_url)
+        except ValidationError as e:
+            return render(request, "sg_url.html",{
+                "error": e.message,
+                "old_value": url
+            })
+        status= True
+        #hash the code using md5
+        hash_code= md5(req_url.encode()).hexdigest()[:10]
+
+        #create host
+        host = request.get_host()
+        #build hash url
+        hash_url = "http://" +host + "/"+ hash_code
+
+        #update data
+        url.hash_url= hash_url
+        url.actual_url= req_url
+        url.save()
+        messages.success(request, "Hey you just updated successfully")
+
+        return redirect("sg_url", id= url.id)
+    return render(request, "sg_url.html", {
+        "url": url
+    })
 
 def index(request):
     if request.method == "POST":
         #init status
-        status =False
+        
         url = request.POST.get("url")
 
         check_url = URLValidator()
@@ -29,7 +67,7 @@ def index(request):
                 "error": e.message,
                 "old_value": url
             })
-        status= True
+        
         #hash the code using md5
         hash_code= md5(url.encode()).hexdigest()[:10]
 
@@ -39,10 +77,8 @@ def index(request):
         hash_url = "http://" +host + "/"+ hash_code
         create_url = Url(hash_url = hash_url, actual_url = url)
         create_url.save()
-
-        return render(request, "home.html", {
-            "status": status
-        })
+        messages.success(request,"Hey You just inserted successfully")
+        return redirect("home")
 
         print(hash_code)
         return render(request, "home.html", {
